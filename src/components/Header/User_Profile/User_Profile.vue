0f29d07ec56c6b215b74c990b6adffcc29b8da84
markosyangrisha <markosyangrisha70@gmail.com>
2024-05-06 13:12:23 +0400

    modified user profile styles

--- src/components/Header/User_Profile/User_Profile.vue
+++ src/components/Header/User_Profile/User_Profile.vue
@@ -1,4 +1,14 @@
 <template>
-	User profile
+	<div class="user_profile">
+		<div class="user-profile_inner">
+			<span class="user_name">Helena</span>
+			<div class="user-profile_photo">
+				<img src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg" alt="user">
+			</div>
+		</div>
+
+	</div>
 </template>
+
+<style scoped src='./User_Profile.css'></style>
 
